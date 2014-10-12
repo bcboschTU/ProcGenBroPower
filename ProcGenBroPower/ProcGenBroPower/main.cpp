ce23a8cfab2c0e589c53eef500296667a24458c6
Marijn Goedegebure <marijngoedegebure@gmail.com>
2014-10-13 00:15:38 +0200

    Added examples for GLFW and GLUT to main.cpp, both installed for the project.

--- ProcGenBroPower/ProcGenBroPower/main.cpp
+++ ProcGenBroPower/ProcGenBroPower/main.cpp
@@ -1,4 +1,123 @@
+// A Simple OpenGL Project
+// Author: Michael Hall
+//
+// This C++ code and project are provided "as is"
+// without warranty of any kind. For personal use only,
+// not for distribution. Copyright 2010 XoaX.
 
-int main() {
+//Include the standard C++ headers  
+#include <stdio.h>  
+#include <stdlib.h> 
+#include <glut.h>
+#include <glfw3.h>
+#define GLFW_INCLUDE_GLU
+
+void Draw() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.25, 0.25, 0.0);
+	glVertex3f(0.75, 0.75, 0.0);
+	glEnd();
+	glFlush();
+}
+
+void Initialize() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int glutExample(int iArgc, char** cppArgv) {
+	glutInit(&iArgc, cppArgv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(200, 200);
+	glutCreateWindow("XoaX.net");
+	Initialize();
+	glutDisplayFunc(Draw);
+	glutMainLoop();
 	return 0;
+}
+
+//Define an error callback  
+static void error_callback(int error, const char* description)
+{
+	fputs(description, stderr);
+	_fgetchar();
+}
+
+//Define the key input callback  
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int glfwExample(void)
+{
+	//Set the error callback  
+	glfwSetErrorCallback(error_callback);
+
+	//Initialize GLFW  
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	//Set the GLFW window creation hints - these are optional  
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Request a specific OpenGL version  
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version  
+	//glfwWindowHint(GLFW_SAMPLES, 4); //Request 4x antialiasing  
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
+
+	//Declare a window object  
+	GLFWwindow* window;
+
+	//Create a window and create its OpenGL context  
+	window = glfwCreateWindow(640, 480, "Test Window", NULL, NULL);
+
+	//If the window couldn't be created  
+	if (!window)
+	{
+		fprintf(stderr, "Failed to open GLFW window.\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	//This function makes the context of the specified window current on the calling thread.   
+	glfwMakeContextCurrent(window);
+
+	//Sets the key callback  
+	glfwSetKeyCallback(window, key_callback);
+
+	//Set a background color  
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+
+	//Main Loop  
+	do
+	{
+		//Clear color buffer  
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Swap buffers  
+		glfwSwapBuffers(window);
+		//Get and organize events, like keyboard and mouse input, window resizing, etc...  
+		glfwPollEvents();
+
+	} //Check if the ESC key had been pressed or if the window had been closed  
+	while (!glfwWindowShouldClose(window));
+
+	//Close OpenGL window and terminate GLFW  
+	glfwDestroyWindow(window);
+	//Finalize and clean up GLFW  
+	glfwTerminate();
+
+	exit(EXIT_SUCCESS);
+}
+int main(int iArgc, char** cppArgv) {
+	// The first method will render a window, the second will not work.
+	glfwExample();
+	glutExample(iArgc, cppArgv);
 }
